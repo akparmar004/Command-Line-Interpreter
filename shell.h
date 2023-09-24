ae9a8d8d4969bb7b4f4a528c114c0c5e0e68f302
akparmar004 <akparmar0404@gmail.com>
2023-09-24 22:59:04 +0530

    Command Line Interpreter Using C

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+void print_prompt1(void);
+void print_prompt2(void);
+
+char *read_cmd(void);
+
+#endif
